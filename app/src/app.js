4670a9a26047e848ec203ee5c4bc83db2e421f72
Mukul Hase <mukulhase@gmail.com>
2016-09-18 12:31:21 +0530

    Updated

--- app/src/app.js
+++ app/src/app.js
@@ -12,8 +12,9 @@ var menus = [{t:"About",l:[
     {t:'About MUNs',l:'about-mun'}
     ]}];
 angular
-    .module('MUN', ['ui.router','ngMaterial', 'MUN.controllers', 'MUN.routes'])
-    .config(function($mdThemingProvider, $mdIconProvider){
+    .module('MUN', ['ui.router','ngMaterial', 'MUN.controllers', 'MUN.routes','sasrio.angular-material-sidenav'])
+    .config(function($$mdSvgRegistry,$mdThemingProvider, $mdIconProvider,ssSideNavSectionsProvider){
+
 
         $mdIconProvider
             .defaultIconSet("./assets/svg/avatars.svg", 128)
@@ -28,9 +29,42 @@ angular
             .primaryPalette('brown')
             .accentPalette('red');
 
-    }).run(function($rootScope){
+        var sections=[];
+        for(var i in links){
+            var temp={
+                'id':'link'+i,
+                'name':links[i].t,
+                'state':links[i].t,
+                'type':'link'
+            };
+            sections.push(temp);
+        }
+        for(i in menus){
+            var temp={
+                id:     'toogle_'+i,
+                name:   menus[i].t,
+                type:   'toggle',
+                pages: []
+            };      
+            for(j in menus[i].l) {
+                temp.pages.push({
+                    id:     'toggle_item_'+i+j,
+                    name:   menus[i].l[j].t,
+                    state:  menus[i].l[j].t
+                });
+            }
+            sections.push(temp);
+        }
+        ssSideNavSectionsProvider.initWithTheme($mdThemingProvider);
+        ssSideNavSectionsProvider.initWithSections(sections);
+
+    }).run(function($rootScope, $mdSidenav, ssSideNav){
         $rootScope.links = links;
         $rootScope.menus = menus;
+        $rootScope.menu = ssSideNav;
+        $rootScope.openMenu = function(){
+            $mdSidenav('left').toggle();
+        };
 
 
     }).config( function($mdThemingProvider){
