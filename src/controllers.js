8d814fbb708ab871151b1977adcafd672aa8a642
mukul hase <mukulhase@gmail.com>
2016-10-07 15:55:24 +0530

    Need to Fix this

--- src/controllers.js
+++ src/controllers.js
@@ -1,7 +1,16 @@
 angular.module('MUN.controllers', [])
 
     .controller('HomeCtrl', function ($scope) {
+    	$(document).ready(function() {
+            $('#fullpage').fullpage({
+                sectionsColor: ['#f2f2f2', '#4BBFC3', '#7BAABE', 'whitesmoke', '#ccddff'],
+                verticalCentered: true,
+  				resize : true,
+  				css3: true,
+  				scrollOverflow:true
 
+            });
+        });
     })
 
     .controller('AboutCtrl', function ($scope) {
