fa0eb70feadae39675b740cb014c74609097517f
mukul hase <mukulhase@gmail.com>
2016-11-02 21:48:00 +0530

    Small changes, added signature

--- src/data.js
+++ src/data.js
@@ -1,7 +1,8 @@
-var letter = "<h1><u> From the Secretary General </u></h1>\
-<p>\
+var letter = "<h1 class=\"md-display-1\"><u> From the Secretary General </u></h1>\
+<div class=\"md-whiteframe-z3 md-body-2\" style= \"background: grey\">\
+<p class='md-headline'>\
 Greetings,\
-<p>\
+</p>\
 It is my utmost pleasure to welcome you to the 3rd edition of IIITH-MUN, from 27 January to 29 January, 2017 at IIIT Hyderabad. IIITH-MUN aims to offer a platform where you can meet a number of talented delegates, and debate on a diverse range of problems relevant on the global scale.\
 <p>\
 IIITH-MUN intends to promote healthy debate, deliberation and cooperation. As you battle it out (intellectually, of course) with the rest of the delegates, IIITH-MUN will expose you to a host of world problems - not just those we face currently, but potential ones that can arise in the future too. It will challenge you to design solutions in a diplomatic manner, through continued discussion and cooperation with delegates representing other countries. \
@@ -12,15 +13,19 @@ This year, we will be hosting the MUN in partnership with CID (Conclave of Inter
 <p>\
 We hope to have an enjoyable and exciting IIITH-MUN, and look forward to welcoming all of you in January 2017.\
 </p>\
-<p>\
-With regards\
-<p>\
+</div>\
+<div>\
+With regards<br>\
+<object type=\"image/svg+xml\" data=\"images/signature.svg\">\
+  Your browser does not support SVG\
+</object>\
+<br>\
 Siddhant Shrivastava,\
 <br>\
 Secretary General,\
 <br>\
 IIITH-MUN, 2017\
-</p>"
+</div>"
 
 var links = [
     {t:"Home",l:"index"},
