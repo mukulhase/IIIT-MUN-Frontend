2ecc4890b9d1c244e1e67565b0987fe2bb4a37b0
Mukul Hase <mukulhase@gmail.com>
2017-01-15 12:50:25 +0530

    AAyn updated

--- src/data.js
+++ src/data.js
@@ -44,7 +44,7 @@ var menus = [{t:"About",l:[
 var secretariat = 
 [
 {'position':'Secretary General','image':'sid.png','name': 'Siddhant Shrivastava', 'pno': '+917799380007', 'email': 'siddhant.shrivastava23@gmail.com'},
-{'position':'Director General','name': 'Aayush Naik','image':'aayush.png', 'pno': '+96567056579', 'email': 'aayushnaik17@gmail.com'},
+{'position':'Director General','name': 'Aayush Naik','image':'aayush.png', 'pno': '+917799271029', 'email': 'aayushnaik17@gmail.com'},
 {'position':'USG Admin','name': 'Aditya Bharti', 'pno': '+919618153254','image':'adi.png', 'email': 'adibhar97@gmail.com'},
 {'name':'Sriram Indurthi','position':'Founder General','image':'Sriram.jpg'},
 {'position':'USG Design','name': 'Pranav Nair', 'pno': '+919642071222','image':'pranav.jpg', 'email': 'pranavnair97@gmail.com'},
